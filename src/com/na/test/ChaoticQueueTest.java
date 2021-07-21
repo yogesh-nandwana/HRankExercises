0deda4b2a0976e7a06c74510877f62082278cb17
yogeshnandwana <yogesh.nandwana@gmail.com>
2021-07-21 10:24:25 +0530

    fix

--- src/com/na/test/ChaoticQueueTest.java
+++ src/com/na/test/ChaoticQueueTest.java
@@ -17,49 +17,49 @@ import java.io.*;
 import java.util.*;
 
 class Test {
-    public static void minimumBribes(List<Integer> list) {
-        Integer[] q = new Integer[list.size()];
-        q =  list.toArray(q);
-        String str = minimumBribes(q,2);
-        System.out.println(str);
-    }
-    private static String minimumBribes(Integer[] q,int perPersonAllowedBribes) {
-        int cnt = 0;
-        for(int i=0;i<q.length;i++) {
-        	int diff = q[i] - (i+1);
-        	if(diff>0) { // if standee is either at right position or ahead 
-        		 System.out.printf("Standee:%d has bribed %d times.\n",q[i],diff);
-                 if(diff > perPersonAllowedBribes) {
-                     return "Too chaotic";
-                 }
-                 if(diff > 0) {
-                     cnt = cnt + diff;    
-                 }
-        	}else if(diff<0) {
-        		 System.out.printf("Standee:%d has eaten %d bribes.\n",q[i],diff);
-        	}
-        }
-        return String.valueOf(cnt);
-    }
+	public static void minimumBribes(List<Integer> list) {
+		Integer[] q = new Integer[list.size()];
+		q =  list.toArray(q);
+		String str = minimumBribes(q,2);
+		System.out.println(str);
+	}
+	private static String minimumBribes(Integer[] q,int perPersonAllowedBribes) {
+		int cnt = 0;
+		for(int i=0;i<q.length;i++) {
+			int diff = q[i] - (i+1);
+			if(diff>0) { // if Standee is either at right position or ahead 
+				System.out.printf("Standee:%d has bribed %d times.\n",q[i],diff);
+				if(diff > perPersonAllowedBribes) {
+					return "Too chaotic";
+				}
+			}
+			for(int j=0;j<i;j++) {
+				if(q[j]>q[i]) {
+					cnt++;
+				}
+			}
+		}
+		return String.valueOf(cnt);
+	}
 }
 
 public class ChaoticQueueTest {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = Integer.parseInt(bufferedReader.readLine().trim());
+		int t = Integer.parseInt(bufferedReader.readLine().trim());
 
-        for (int tItr = 0; tItr < t; tItr++) {
-            int n = Integer.parseInt(bufferedReader.readLine().trim());
-            String[] qTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-            List<Integer> q = new ArrayList<>();
+		for (int tItr = 0; tItr < t; tItr++) {
+			int n = Integer.parseInt(bufferedReader.readLine().trim());
+			String[] qTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+			List<Integer> q = new ArrayList<>();
 
-            for (int i = 0; i < n; i++) {
-                int qItem = Integer.parseInt(qTemp[i]);
-                q.add(qItem);
-            }
-            Test.minimumBribes(q);
-        }
-        bufferedReader.close();
-    }
+			for (int i = 0; i < n; i++) {
+				int qItem = Integer.parseInt(qTemp[i]);
+				q.add(qItem);
+			}
+			Test.minimumBribes(q);
+		}
+		bufferedReader.close();
+	}
 }

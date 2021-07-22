5b62998793770cab5e9973c2ad1a9b4327615099
yogeshnandwana <yogesh.nandwana@gmail.com>
2021-07-22 17:37:48 +0530

    fix

--- src/com/na/test/PairMatchTest.java
+++ src/com/na/test/PairMatchTest.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class PairMatchTest {
 	public static void main(String[] args) {
 		int[] ar = {1,2,1,2,1,3,2};
+		//int[] ar = {10, 20, 20, 10, 10, 30, 50, 10, 20};
 		Arrays.sort(ar);
 		System.out.println(Arrays.toString(ar));      
 		int i=0,j=1,count=0;
@@ -13,9 +14,10 @@ public class PairMatchTest {
 				System.out.printf("Pair matched at:(%d,%d)\n",i,j);
 				count++;
 				i=i+2;
-				j=i+2;
+				j=i+1;
 			}else {
 				i++;
+				j++;
 			}
 		}
 		System.out.println("count:"+count);
